b7074d13a82ef89fd9d2aa3aa95565b17dc8a5d5
shu_0 <shu.kwn0257b@gmail.com>
2022-09-10 01:49:01 +0900

    install tailwind in mix.exs

--- mix.exs
+++ mix.exs
@@ -48,7 +48,9 @@ defmodule TrDaisy3.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+
+      {:phx_gen_tailwind, "~> 0.1.3", only: :dev} # 追加
     ]
   end
 
